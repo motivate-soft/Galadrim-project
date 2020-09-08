8af31771bd2a4de96ed0a4a839d70a5c019d21e2
Galadrim VPS <galadrim_vps@gmail.com>
2020-09-08 16:49:03 +0200

    Material icon add

--- src/components/header.js
+++ src/components/header.js
@@ -3,17 +3,16 @@ import Logo from '../components/logo';
 import Navigation from '../components/navigation';
 
 
-
 export default function Header() {
 
 	return (
 		<div style={{
 			'display': 'flex',
-			'align-items': 'center',
-			'justify-content': 'space-between',
+			'alignItems': 'center',
+			'justifyContent': 'space-between',
 			'width': 'calc(100% - 30px)',
 			'margin': '5px 15px',
-			'border-bottom': '1px solid #eee'
+			'borderBottom': '1px solid #eee'
 		}}>
 			<div style={{ display: "inline-block" }}>
 				<div className="brand-logo">
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,27 +1,18 @@
 import React from 'react';
 // import { makeStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
-
 
 
 export default function Navigation() {
 
   return (
-    <div class="topnav" id="myTopnav">
-      <a href="#home" class="active">Home</a>
-      <a href="#news">News</a>
-      <a href="#contact">Contact</a>
-      <div class="dropdown">
-        <button class="dropbtn">Dropdown
-      <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-          <Link href="#">Link 1</Link>
-          <Link href="#">Link 2</Link>
-          <Link href="#">Link 3</Link>
-        </div>
-      </div>
-      <Link href="#about">About</Link>
+    <div className="topnav" id="myTopnav">
+      <a href="#home" className="active">Qui sommes nous ?</a>
+      <a href="#news">Nos expertises</a>
+      <a href="#contact">Nos partenariats</a>
+      <a href="#contact">Efisens Clic&Shop</a>
+      <a href="#contact">Efisens Lab</a>
+      <a href="#contact">Carrières</a>
+      <a href="#contact">Nous contacter</a>
     </div>
   );
 }
