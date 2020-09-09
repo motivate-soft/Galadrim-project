c25e53848c417b2403d0068f4752f1616fce5012
Administrator <inhedeveloper@hotmail.com>
2020-09-09 16:50:50 +0800

    dropdown menu added

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Logo from '../components/logo';
 
+import { FaAngleDown } from '../../node_modules/react-icons/fa';
+import Cloud from '../assets/home/images/cloud.png'
+import Workspace from '../assets/home/images/modern-workplace.png'
+import Service from '../assets/home/images/s-curit.png'
+import ServiceManage from '../assets/home/images/service-manag.png'
+
 
 export default function Header() {
 
@@ -20,7 +26,64 @@ export default function Header() {
                             <a className="nav-link" href="#">Qui sommes nous ?</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Nos expertises</a>
+                            <a className="nav-link" href="#">Nos expertises <FaAngleDown/></a>
+                            <div className="dropdown-content">
+                                <div className="card card-body">
+                                    <div className="tail">
+
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-lg-6">
+                                            <a href="">
+                                                <div className="media">
+                                                    <img className="align-self-start mr-3" src={Cloud}
+                                                         alt="Cloud"/>
+                                                        <div className="media-body">
+                                                            <h5 className="mt-0">Top-aligned media</h5>
+                                                            <p>Adopter des solutions cloud hybrides</p>
+                                                        </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <div className="col-lg-6">
+                                            <a href="">
+                                                <div className="media">
+                                                    <img className="align-self-start mr-3" src={Workspace}
+                                                         alt="Cloud"/>
+                                                    <div className="media-body">
+                                                        <h5 className="mt-0">Modern Workplace</h5>
+                                                        <p>Moderniser vos environnements de travail</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <div className="col-lg-6">
+                                            <a href="">
+                                                <div className="media">
+                                                    <img className="align-self-start mr-3" src={Service}
+                                                         alt="Cloud"/>
+                                                    <div className="media-body">
+                                                        <h5 className="mt-0">Sécurité</h5>
+                                                        <p>Protéger vos données, votre infrastructure et vos utilisateurs</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <div className="col-lg-6">
+                                            <a href="">
+                                                <div className="media">
+                                                    <img className="align-self-start mr-3" src={ServiceManage}
+                                                         alt="Cloud"/>
+                                                    <div className="media-body">
+                                                        <h5 className="mt-0">Services Managés</h5>
+                                                        <p>Superviser et surveiller votre système IT</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">Nos partenariats</a>
