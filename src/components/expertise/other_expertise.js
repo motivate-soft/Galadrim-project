7a2df1461410dd08151f973e9e6a7797e28248ca
Administrator <inhedeveloper@hotmail.com>
2020-09-12 00:41:26 +0800

    update cloud page

--- src/components/expertise/other_expertise.js
+++ src/components/expertise/other_expertise.js
@@ -1,6 +1,71 @@
 import React from 'react';
+import Cloud from "../../assets/home/images/cloud.png";
+import Workspace from "../../assets/home/images/modern-workplace.png";
+import Service from "../../assets/home/images/s-curit.png";
+import IconButtonCard from "../global/Icon_button_card";
+
+import  FormesImageOrigin from '../../assets/expertise/cloud/orange-mini.svg';
+import  FormesImageGreen from '../../assets/expertise/cloud/vert-mini.svg';
+import  FormesImageBlue from '../../assets/expertise/cloud/bleu-mini.svg';
+
+const items = [
+    {
+        id: '1',
+        img: Cloud,
+        title: 'Cloud',
+        text: 'Optimiser l’utilisation de votre IT au quotidien avec des technologies agiles, sécurisées et innovantes.',
+        buttonText: 'En savoir plus',
+        active: false,
+    },
+    {
+        id: '2',
+        img: Workspace,
+        title: 'Modern Workplace',
+        text: 'Mettre en place et interconnecter des solutions collaboratives pour vos équipes.',
+        buttonText: 'En savoir plus',
+        active: false,
+    },
+    {
+        id: '3',
+        img: Service,
+        title: 'Sécurité',
+        text: 'Vous permettre d’être serein en protégeant votre cœur informatique ainsi que vos données  tout en garantissant la protection des données utilisateurs (RGPD friendly).',
+        buttonText: 'En savoir plus',
+        active: false,
+    },
+
+];
 
 class OtherExpertise extends React.Component{
+
+    state = {
+        items: items
+    }
+
+    handleMouseEnter = (id) => {
+        const { items } = this.state;
+        items.forEach((item) => {
+            if (item.id === id) {
+                item.active = true
+            } else {
+                item.active = false
+            }
+        })
+
+        this.setState({ items: items })
+    }
+
+    handleMoustLeave = (id) => {
+        const { items } = this.state;
+        items.forEach((item) => {
+            if (item.id === id) {
+                item.active = false
+            }
+        })
+
+        this.setState({ items: items })
+    }
+
     render()
     {
         return (
@@ -15,6 +80,29 @@ class OtherExpertise extends React.Component{
                             <p style={{color: this.props.theme.mainColor}}>
                                 Nous agrégeons les meilleures technologies IT pour qu’elles puissent vous être bénéfiques au quotidien.s
                             </p>
+                        </div>
+                    </div>
+                    <div className={'col-lg-12'}>
+                        <div className={'row card-area'}>
+                            <div className={'top-right-img'}/>
+                            <img src={FormesImageGreen} alt="" className={'bottom-right-img'}/>
+                            <img src={FormesImageBlue} alt="" className={'top-left-img'}/>
+                            {
+                                items.map((item, index) => (
+                                    <div key={index} className="col-md-6 col-lg-3">
+                                        <IconButtonCard
+                                            id={item.id}
+                                            active={item.active}
+                                            img={item.img}
+                                            title={item.title}
+                                            text={item.text}
+                                            buttonText={item.buttonText}
+                                            onMouseEnter={this.handleMouseEnter}
+                                            onMouseLeave={this.handleMoustLeave}
+                                        />
+                                    </div>
+                                ))
+                            }
                         </div>
                     </div>
                 </div>
