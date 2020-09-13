b5383733f1e60f35c838e5e20446d7a3244eca70
Administrator <inhedeveloper@hotmail.com>
2020-09-14 04:24:32 +0800

    our approach added

---
+++ src/components/partner_ship/in_news.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+export default function InNews() {
+    return (
+        <div className={'in_news'}>
+            <div className={'container'}>
+                <div className={'row'}>
+                    <div className={'col-lg-8 ml-auto mr-auto text-center'}>
+                        <div className={'title-area'}>
+                            <h1>C'est au contact d'experts techniques, de leaders technologiques et de partenaires stratégiques que nous innovons ! </h1>
+                            <p/>
+                        </div>
+                    </div>
+                    <div className={'col-lg-10 ml-auto mr-auto card-area'}>
+                        <div className={'wide-card'}>
+                            <div className={'row'}>
+                                <div className={'col-lg-6'}>
+                                    <div className={'image-part'}>
+                                        <img src="" alt=""/>
+                                    </div>
+                                    <img src="" alt=""/>
+                                </div>
+                                <div className={'col-lg-6'}>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
