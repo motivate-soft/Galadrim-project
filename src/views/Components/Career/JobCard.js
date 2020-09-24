f434fc8b803eb26f69372cd4a59ca2bfe71a31d8
robert-upwork <robyop.eyeco@gmail.com>
2020-09-24 13:44:41 +0700

    fix career page category badge

--- src/views/Components/Career/JobCard.js
+++ src/views/Components/Career/JobCard.js
@@ -8,36 +8,25 @@ export default function JobCard(props) {
     const history = useHistory()
 
     function getJobBadge(category) {
-        console.log(category)
-        if (category === 'Tech') {
-            console.log('Tech', category)
-            return 'badge-green'
-        } else if (category === 'Marketing') {
-            console.log('market', category)
-            return 'badge-orange'
-        } else {
-            console.log(category)
-            return 'badge-blue'
+        switch (category) {
+            case 'Tech':
+                return 'badge-green';
+            case 'Marketing':
+                return 'badge-orange';
+            case 'Administration':
+                return 'badge-blue';
+            default:
+                return 'badge-green';
         }
-        // switch (category) {
-        //     case 'Tech':
-        //         return 'badge-green';
-        //     case 'Marketing':
-        //         return 'badge-orange';
-        //     case 'Administration':
-        //         return 'badge-blue';
-        //     default:
-        //         return 'badge-green';
-        // }
     }
     return (
-        <div className={`career-job-card ${getJobBadge(category)}`} onClick={() => history.push(`/career/${id}`)}>
-            <span className="job-badge badge-green">{category}</span>
+        <div className="career-job-card" onClick={() => history.push(`/career/${id}`)}>
+            <span className={category ? `job-badge ${getJobBadge(category)}` : `job-badge badge-green`}>{category}</span>
             <h4>{title}</h4>
             <div className="job-location">
                 <span className="job-location-badge"><FaMapMarkerAlt />Paris</span>
                 <span className="job-location-badge"><FaRegClock />CDI</span>
             </div>
-        </div>
+        </div >
     )
 }
