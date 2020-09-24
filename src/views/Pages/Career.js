3b51abca85aab4a9ad2d989658a6ab6290d503f9
robert-upwork <robyop.eyeco@gmail.com>
2020-09-24 12:03:48 +0700

    import issues

--- src/views/Pages/Career.js
+++ src/views/Pages/Career.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import CareerOffer from '../Components/Career/CareerOffer'
-import { CareerBanner } from '../views/Components/Career/CareerBanner';
+import CareerBanner from '../Components/Career/CareerBanner';
 
 export default function Career() {
     return (
