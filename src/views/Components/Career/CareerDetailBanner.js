3486f716e87cb1de9bf82f2cd02c31d0dc7f738c
robert-upwork <robyop.eyeco@gmail.com>
2020-09-24 12:39:35 +0700

    fix banner

--- src/views/Components/Career/CareerDetailBanner.js
+++ src/views/Components/Career/CareerDetailBanner.js
@@ -8,6 +8,7 @@ export default function CareerDetailBanner() {
     return (
         <div className="section career-detail-banner">
             <img src={LeftTopImage} alt="Left Top Background" className="top-left" />
+            {/* <img src={LeftTopImage} alt="Left Top Background" className="top-right" /> */}
             <div className="container">
                 <div className="career-container d-flex flex-column">
                     <h2>{category}</h2>
