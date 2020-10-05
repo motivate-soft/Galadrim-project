60a1a8abe99dc10d806f765afb7e8a53c14b713c
robert-upwork <robyop.eyeco@gmail.com>
2020-10-05 09:34:41 +0700

    fix articles card

--- src/views/Components/Expertise/Resources.js
+++ src/views/Components/Expertise/Resources.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import VGradientCard from '../Common/VGradientCard';
 import ArticleCard from '../Common/ArticleCard';
-import { RightRoundedSemiCircle } from '../Elements/Drawings';
 
 
 export default function Resources(props) {
@@ -9,17 +8,17 @@ export default function Resources(props) {
 
     return (
         <div className="section expertise-resources-block">
-            <RightRoundedSemiCircle width={55} height={110} className={color ? `bottom-left transform-bottom-50 bg-${color}` : `bottom-left transform-bottom-50 bg-green`} />
-            <div className="container">
-                <h1 className={color ? `block-title text-${color}` : `block-title text-green`}>Ressources associées</h1>
-                <div className="articles-container">
+            <div className={color ? `right-rounded-semi-circle bottom-left transform-bottom-50 bg-${color}` : `right-rounded-semi-circle bottom-left transform-bottom-50 bg-green`} />
+            <h1 className={color ? `block-title text-${color}` : `block-title text-green`}>Ressources associées</h1>
+            <div className="articles-container">
+                <div className="container">
                     <img src={rightMini} alt="right mini" className="top-right" />
                     <img src={leftMini} alt="left mini" className="bottom-left" />
                     <div className="row">
                         {
                             articles.map((article, index) => (
                                 <div key={index} className="col-12 col-sm-12 col-md-6 col-lg-3 mb-4">
-                                    <VGradientCard maxWidth={276}>
+                                    <VGradientCard>
                                         <ArticleCard
                                             key={index}
                                             id={article.id}
